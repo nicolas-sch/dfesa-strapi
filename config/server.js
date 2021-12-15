3a548321021d86695327604eba37a6b511c8e086
nicolas-sch <nicolasdrivebackup@gmail.com>
2021-12-15 15:30:53 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b8bd91262c3b8ee5546cc287c8d7d907'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4552441f-450b-4749-b8f8-3e688923084f'
+};
